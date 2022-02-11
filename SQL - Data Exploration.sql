bd62745bfdb4bd94f5f5f3642b58e393cf7650be
Ekta Manvar <66343222+Ekta-Manvar@users.noreply.github.com>
2022-02-11 10:18:52 -0500

    Update SQL - Data Exploration.sql

--- SQL - Data Exploration.sql
+++ SQL - Data Exploration.sql
@@ -171,10 +171,14 @@ ORDER BY location
 
  SELECT d.continent AS Continent, d.location AS Country, 
         d.date AS Date, d.population AS Population, 
-         ISNULL(d.new_cases,0) AS Total_Cases, ISNULL(d.new_deaths, 0) AS Total_Deaths,  
-		 ISNULL(d.hosp_patients,0) AS Hospitalized, ISNULL(d.icu_patients,0) AS ICU_Patients, 
-		 ISNULL(v.total_vaccinations,0) AS Total_Vaccinations, ISNULL(v.people_vaccinated,0) AS People_Vaccinated, 
-		 ISNULL(v.people_fully_vaccinated,0) AS People_Fully_Vaccinated, ISNULL(v.total_boosters,0) AS Total_Boosters
+         ISNULL(d.new_cases,0) AS Total_Cases, 
+         ISNULL(d.new_deaths, 0) AS Total_Deaths,  
+         ISNULL(d.hosp_patients,0) AS Hospitalized, 
+         ISNULL(d.icu_patients,0) AS ICU_Patients, 
+         ISNULL(v.total_vaccinations,0) AS Total_Vaccinations, 
+         ISNULL(v.people_vaccinated,0) AS People_Vaccinated, 
+	 ISNULL(v.people_fully_vaccinated,0) AS People_Fully_Vaccinated, 
+         ISNULL(v.total_boosters,0) AS Total_Boosters
   FROM portfolio.dbo.CovidDeaths d
   JOIN portfolio.dbo.CovidVaccinations v
   ON d.date=v.date and d.location=v.location
